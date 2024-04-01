2135d1d44a07917446d88bfb5b2f70b50745f050
Mohammed Fazil <fazelcm2000@gmail.com>
2024-04-01 17:44:39 +0530

    Core completed

--- src/components/products/Catglass.jsx
+++ src/components/products/Catglass.jsx
@@ -1,33 +1,71 @@
 import React, { useState } from 'react';
 import ProductImages from '../../mockdata/images';
+import ProductModels from '../../mockdata/models';
+import { Environment, OrbitControls, useGLTF } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+
+const ModelTest = ({ src }) => {
+  const [error, setError] = useState(null);
+  const { scene } = useGLTF(src, '', setError);
+
+  // If an error occurs during loading, display an error message
+  if (error) {
+    console.error('Error loading 3D model:', error);
+    return <p>Error loading 3D model. Please try again later.</p>;
+  }
+
+  return <primitive object={scene} />;
+};
 
 const Catglass = () => {
   const [selectedImage, setSelectedImage] = useState(ProductImages[0].url);
+  const [selectedModel, setSelectedModel] = useState(ProductModels[0]);
 
   const handleImageSelection = (imageUrl) => {
     setSelectedImage(imageUrl);
   };
 
+  const handleModelSelection = (model) => {
+    setSelectedModel(model);
+  };
+
   return (
     <div className='flex justify-around h-screen mt-10'>
       <div className='border-1 w-1/3 h-[60vh]'>
-      <img src={selectedImage} alt="Selected Image" />
-      <div className=' flex justify-center'>
-        {ProductImages.map((image, index) => (
-          <img
-            key={index}
-            src={image.url}
-            alt={`Image ${index}`}
-            className= ' w-10 h-10 border-black p-1 mx-1 border-2 rounded-full'
-            onMouseEnter={() => handleImageSelection(image.url)}
-          />
-        ))}
+        <img src={selectedImage} alt="Selected Image" />
+        <div className='flex justify-center'>
+          {ProductImages.map((image, index) => (
+            <img
+              key={index}
+              src={image.url}
+              alt={`Image ${index}`}
+              className={'w-10 h-10 border-black p-1 mx-1 border-2 rounded-full'}
+              onMouseEnter={() => handleImageSelection(image.url)}
+            />
+          ))}
         </div>
       </div>
       <div className='border-1 w-1/3 h-[60vh]'>
-       2
+        <Canvas>
+          <OrbitControls />
+          <Environment preset='studio' />
+          <ModelTest src={selectedModel.src} />
+        </Canvas>
+        <div className='flex justify-center'>
+          {ProductModels.map((model, index) => (
+            <img
+              key={index}
+              src={model.img}
+              className={'w-10 h-10 border-black p-1 mx-1 border-2 rounded-full'}
+              onMouseEnter={() => handleModelSelection(model)}
+            />
+          ))}
+        </div>
       </div>
-      <div className='border-1 w-1/4 h-[60vh]'>3</div>
+      <div className='border-1 w-1/4 h-[60vh]'>
+        <h1 className=' font-bold'>Price Details</h1>
+        <button className=' bg-blue-500 text-white p-2 rounded-md'><a href='https://www.effeconsultancy.com/computer-glasses' target='blank'>Virtual TryOn</a></button>
+      </div>
     </div>
   );
 };
---
+++ src/mockdata/models.js
@@ -0,0 +1,29 @@
+const ProductModels = [
+    {
+      id: 1,
+      color: "Red",
+      src: "/cateyemod.glb",
+      img:"https://ik.imagekit.io/orm5ahbvhz/D-similar/1.png?updatedAt=1711627980128"
+    },
+    {
+      id: 2,
+      color: "Blue",
+      src: "/cateyeglass1.glb",
+      img:"https://ik.imagekit.io/orm5ahbvhz/D-similar/1.png?updatedAt=1711627980128"
+    },
+    {
+      id: 3,
+      color: "Yellow",
+      src: "/cateyeglass.glb",
+      img:"https://ik.imagekit.io/orm5ahbvhz/D-similar/1.png?updatedAt=1711627980128"
+    },
+    {
+      id: 4,
+      color: "Green",
+      src: "/cateyemod.glb",
+      img:"https://ik.imagekit.io/orm5ahbvhz/D-similar/1.png?updatedAt=1711627980128"
+    }
+  ];
+  
+  export default ProductModels;
+  
