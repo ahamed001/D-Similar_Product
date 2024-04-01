fb007ba256a5a99c914dbc409dbb4f7b4f75d62c
Ahamed <ahamed.irfan001@gmail.com>
2024-04-01 12:25:53 +0530

    updated

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import glasses from '../assets/glasses.jpg'
+import furniture from "../assets/furnitures.jpg"
 
 export const Landing = () => {
   return (
     <div className=' flex justify-center items-center h-[100vh] w-full'>
       <Link to={`catglass`}>
-        <button className=' bg-red-500 text-white p-3 font-bold rounded-full mx-4'> Test-1 </button>
+        <img
+          src="https://ik.imagekit.io/orm5ahbvhz/D-similar/glasses.jpg?updatedAt=1711954401211"
+          alt="product" />
+        {/* <button className=' bg-red-500 text-white p-3 font-bold rounded-full mx-4'> Test-1 </button> */}
       </Link>
       <Link to={`scific`}>
-        <button className=' bg-red-500 text-white p-3 font-bold rounded-full'> Test-2 </button>
+        <img
+          src="https://ik.imagekit.io/orm5ahbvhz/D-similar/furnitures.jpg?updatedAt=1711954378722"
+          alt="product" />
+        {/* <button className=' bg-red-500 text-white p-3 font-bold rounded-full'> Test-2 </button> */}
       </Link>
     </div>
   )
