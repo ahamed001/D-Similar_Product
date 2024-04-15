c647b8ad78340adb79896b72f2df4523f0d492a4
Mohammed Fazil <fazelcm2000@gmail.com>
2024-04-15 16:20:34 +0530

    Style Update

--- src/mockdata/images.js
+++ src/mockdata/images.js
@@ -18,12 +18,7 @@ const ProductImages = [
       id: 4,
       color: "Green",
       url: "https://ik.imagekit.io/orm5ahbvhz/D-similar/4.png?updatedAt=1711628054176"
-    },    
-    {
-      id: 5,
-      color: "Size",
-      url: "https://ik.imagekit.io/orm5ahbvhz/D-similar/size.jpg?updatedAt=1712382704443"
-    },    
+    },      
   ];
   
 export default ProductImages;
